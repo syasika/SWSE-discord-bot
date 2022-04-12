f69e3e71ecc74db5afaa3e7e0df01cedba4671eb
Shane <shaneyasika82@gmail.com>
2022-04-11 23:03:09 -0400

    add number range randomizer

--- cogs/roll/cog.py
+++ cogs/roll/cog.py
@@ -1,3 +1,4 @@
+import random
 from discord import Embed
 import nextcord
 from nextcord.ext import commands
@@ -58,8 +59,50 @@ class Roll(commands.Cog, name="Roll"):
             )
 
 
+class Randomize(commands.Cog, name="Randomize"):
+    """Randomizes a list between provided range"""
+
+    COG_EMOJI = "🎲"
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("Randomize command ready")
+
+    @commands.command(name="randomize")
+    async def roll(self, ctx, *arg):
+        """Randomizes a list between provided range
+
+        Usage:
+        ```
+        $randomize 1 9
+        ```
+        """
+        try:
+            result = get_randomized_list(arg[0], arg[1])
+            await ctx.send(embed=embed_success(result))
+
+        except Exception as e:
+            print(f"Error during randomize {arg}: {str(e)}")
+            await ctx.send(
+                "https://tenor.com/view/parks-and-rec-who-broke-gif-23407071"
+            )
+            await ctx.send(
+                embed=embed_error("Don't know what you did, but you've been reported!")
+            )
+
+
 def setup(client):
     client.add_cog(Roll(client))
+    client.add_cog(Randomize(client))
+
+
+def get_randomized_list(start, end):
+    start_list = list(range(int(start), (int(end) + 1)))
+    randomized_list = random.sample(start_list, len(start_list))
+    return randomized_list
 
 
 def get_roll(dice_roll_text):
